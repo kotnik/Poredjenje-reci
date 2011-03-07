f088ca817fcb7f4e85cd82cdd2d5ebf537f35f12
Nikola Kotur <kotnick@gmail.com>
2011-03-07 21:02:02 +0100

    Parsiranje Get parametra u JavaScriptu

--- js/script.js
+++ js/script.js
@@ -12,8 +12,19 @@ $(document).ready(function() {
 
   $("a.rec").live('click', function() {
     var word_id = this.id;
-    // FIXME: unlock for sets other that 1
-    $("#words").html(ajax_load).load(loadUrl, "wordid=" + word_id + "&set=1");
+    var get_set = gup('set');
+    $("#words").html(ajax_load).load(loadUrl, "wordid=" + word_id + "&set=7");
   });
 
 });
+
+function gup(name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return results[1];
+}
